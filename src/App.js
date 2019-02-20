6434b5a2ca54a58323392e70f064afaa8bca9278
Vectormike40 <victor.junior40@yahoo.com>
2019-02-20 13:15:34 +0100

    Added face detection model

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageForm from './components/ImageForm/ImageForm'
 import './App.css' 
+
+
+const app = new Clarifai.App({
+  apiKey: '03a416607bf24a56837f89e9744b5ea5'
+ });
+
 
 class App extends Component {
   constructor() {
@@ -13,12 +20,24 @@ class App extends Component {
     }
   }
 
+  
+
   OnInputChange = (event) => {
     console.log(event.target.value)
   }
 
   onButtonSubmit = () => {
     console.log('Click!')
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+      console.log(err, `There was an error`)
+    }
+  );
   }
 
   render() {
@@ -26,7 +45,7 @@ class App extends Component {
       <div className="App">
         <Navigation/>
         <Logo/>
-        <ImageForm onButtonSubmit={this.OnButtonSubmit} OnInputChange={this.OnInputChange}/>
+        <ImageForm onButtonSubmit={this.onButtonSubmit} OnInputChange={this.OnInputChange}/>
       </div>
     );
   }
