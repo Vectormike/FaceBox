215e66c88b54d2e8d11fe402af3532f89d53589e
Vectormike40 <victor.junior40@yahoo.com>
2019-02-25 10:54:46 +0100

    Made some conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ImageForm from './components/ImageForm/ImageForm'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css' 
 
-
+// Saved api key as app
 const app = new Clarifai.App({
   apiKey: '03a416607bf24a56837f89e9744b5ea5'
  });
@@ -32,15 +32,13 @@ class App extends Component {
   onButtonSubmit = () => {
     //Set input 
     this.setState(
-      {
-        imageUrl: this.state.input
-      }
-      )
+      {imageUrl: this.state.input}
+    )
+
     //Trigger predict model
     app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input 
-      )
+      Clarifai.FACE_DETECT_MODEL, this.state.input 
+    )
     .then(
     function(response) {
       // do something with response
@@ -54,12 +52,18 @@ class App extends Component {
   }
 
   render() {
+    let image;
+    (this.state.imageUrl.length === 0) ?
+    image = <p className='bg-gray br3 f3 grow dim pa2 ma5'>Your image will show here, mate</p> :
+    image = <FaceRecognition imageUrl={this.state.imageUrl}/>
+
+
     return (
       <div className="App">
         <Navigation/>
         <Logo/>
         <ImageForm onButtonSubmit={this.onButtonSubmit} OnInputChange={this.OnInputChange}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        {image}        
       </div>
     );
   }
