a4ae3a7a36af0a18b13279c87fe8946b5bbcc733
Vectormike40 <victor.junior40@yahoo.com>
2019-03-08 17:33:30 +0100

    Styled

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,46 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <div class="bg-blue h-100">
-
-	<div className="pv4 mid-gray sans-serif">
-		<div className="w-90 w-80-m w-50-l center cf cover bg-top bg-center shadow-1 bg-light-gray" style={{
-            background-image: url('https://source.unsplash.com/MTJxRri1UiI/800x600')
-        }}>
-
-			<div class="w-80 w-70-m w-60-l fr bg-white">
-				<header class="pa3 bb b--light-gray relative">
-					<h1 class="b ma0 f3">Sign Up</h1>
-				</header>
-				<form id="register-form" action="" class="ph3 pt3 pb4 f7">
-					<div class="mb3">
-						<label for="" class="db ttu b lh-copy">Username</label>
-						<input name="username" type="text" class="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
-					</div>
-					<div class="mb3">
-						<label for="" class="db ttu b lh-copy">Email</label>
-						<input name="email" type="email" class="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
-					</div>
-					<div class="mb4">
-						<label for="" class="db ttu b lh-copy">Password</label>
-						<input name="password" type="password" class="input-reset w-100 mw-100 bn br1 pa2 bg-light-gray" />
-					</div>
-					<div class="mb3 tc f6">
-						Do you have an account? <Link to="/login" className="blue ph1">Log In Now!</Link>
-					</div>
-					<div class="tc">
-						<input type="submit" value="Sign Up" class="ttu bn pv3 ph4 f6 bg-blue white b br-pill pointer grow" />
-					</div>
-				</form>
-			</div>
-		</div>
-		<div class="tc white mt4 f6">
-			Based on <a target="_blank" rel="noopener" href="https://dribbble.com/shots/3150554-Sign-Up" class="lightest-blue">this shot</a>.
-		</div>
-	</div>
-
-</div>
+        <article className="br4 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+            <main className="pa4 black-80">
+                <form className="measure center">
+                    <fieldset className="ba b--transparent ph0 mh0">
+                        <legend className="f4 fw6 ph0 mh0">Register</legend>
+                        <div className="mt3">
+                            <label 
+                            className="db fw6 lh-copy f6" 
+                            htmlFor="email-address">Email
+                            </label>
+                            <input 
+                            className="pa2 br3 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" 
+                            name="email-address"
+                            />
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw6 lh-copy f6">Password</label>
+                            <input className="b br3 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                        </div>
+						<div className="mv3">
+                            <label className="db fw6 lh-copy f6">Confirm Password</label>
+                            <input className="b br3 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                        </div>
+                    </fieldset>
+                    <div className="">
+                        <Link to="/app" 
+                            className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-blue"  
+                            type="submit">
+                            Register
+                        </Link>
+                    </div>
+                    <div class="lh-copy mt3">
+                        <Link to="/login" className="f6 link dim black db">Login</Link>
+                    </div>
+                </form>
+            </main>
+        </article>
     )
 }
 
